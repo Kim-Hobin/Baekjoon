8e89025829ac9a8cb1c32a78785a08d3abd7ef22
Kim Hobin <20141689@g.dongseo.ac.kr>
2021-05-06 17:15:02 +0900

    init commit

--- Baekjoon/1011.cpp
+++ Baekjoon/1011.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-
 int main() {
+	int n;
+	cin >> n;
 
-	
-	
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+
+		int move, max = 0;
+
+		while (max * max <= y - x) {
+			max++;
+		}
+		max--;
+		move = 2 * max - 1;
+		long long a = (y - x) - max * max;
+		a = (long long)ceil((double)a / (double)max);
+		move += a;
+		cout << move << "\n";
+	}
+
 	return 0;
 }
