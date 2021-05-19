714dc1ab56685815ddf5f2c73cb417a0de048f4a
Kim Hobin <20141689@g.dongseo.ac.kr>
2021-05-19 11:28:00 +0900

    init commit

--- Baekjoon/1149.cpp
+++ Baekjoon/1149.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-// DG
-// EC
 using namespace std;
 
+const int MAX = 1001;
 
-int main () {
+
+int cost[MAX][4];
+int cache[1001][4];
+
+int F(int num) {
+	for (int i = 1; i <= 3; i++) {
+		cache[1][i] = cost[1][i];
+	}
+	for (int i = 2; i <= num; i++) {
+		for (int j = 1; j <= 3; j++) {
+			cache[i][j] = cost[i][j] + min(cache[i - 1][j % 3 + 1], cache[i - 1][(j + 1) % 3 + 1]);
+		}
+	}
+	return min({ cache[num][1], cache[num][2], cache[num][3] });
+}
+
+int main() {
+	int N; cin >> N; // ÁýÀÇ ¼ö
+	
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= 3; j++) {
+			cin >> cost[i][j];
+		}
+	}
+
+	cout << F(N) << endl;
 
 	return 0;
 }
