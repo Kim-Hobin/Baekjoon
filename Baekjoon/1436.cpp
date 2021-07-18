4bd0ec9e27b2233d72fd93ddff5a2892550b7a94
Kim Hobin <20141689@g.dongseo.ac.kr>
2021-07-18 17:48:11 +0900

    init commit

--- Baekjoon/1436.cpp
+++ Baekjoon/1436.cpp
@@ -3,7 +3,37 @@
 using namespace std;
 
 int main() {
+	int N;
+	cin >> N;
+	int i = 666;
 
+	while (1) {
+		int part = i;
+		int cnt = 0;
+
+		while (part) {
+			if (part % 10 == 6) {
+				cnt++;
+				if (cnt == 3) {
+					break;
+				}
+			}
+			else {
+				if (cnt) {
+					cnt = 0;
+				}
+			}
+			part /= 10;
+		}
+		if (cnt >= 3) {
+			N--;
+			if (!N) {
+				cout << i << endl;
+				break;
+			}
+		}
+		i++;
+	}
 
 	return 0;
 }
