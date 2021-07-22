d1412aa2c9bcdb632a36f8a80742a42317903daf
Kim Hobin <20141689@g.dongseo.ac.kr>
2021-07-22 20:49:03 +0900

    init commit

--- Baekjoon/11651.cpp
+++ Baekjoon/11651.cpp
@@ -13,12 +13,17 @@ void merge(int left, int mid, int right) {
 	j = mid + 1;
 	k = left;
 
-	while (i <= mid && j <= right) { // 분할되어 정렬이 이루어진 list의 합병
-		if (list[i].first < list[j].first) {
+	while (i <= mid && j <= right) {
+		if (list[i].second < list[j].second) {
 			sorted[k++] = list[i++];
 		}
-		else if ((list[i].first == list[j].first) && (list[i].second < list[j].second)) {
-			sorted[k++] = list[i++];
+		else if (list[i].second == list[j].second) {
+			if (list[i].first < list[j].first) {
+				sorted[k++] = list[i++];
+			}
+			else {
+				sorted[k++] = list[j++];
+			}
 		}
 		else {
 			sorted[k++] = list[j++];
@@ -36,7 +41,7 @@ void merge(int left, int mid, int right) {
 		}
 	}
 
-	for (l = left; l <= right; l++) { // 임시 배열 sorted[]의 리스트를 배열 list[]로 재복사
+	for (l = left; l <= right; l++) {
 		list[l] = sorted[l];
 	}
 }
@@ -44,11 +49,11 @@ void merge(int left, int mid, int right) {
 void merge_sort(int left, int right) {
 	int mid;
 
-	if (left < right) { // 기저사례, 리스트의 요소가 1개가 남을 때까지 분할
-		mid = (left + right) / 2; 
-		merge_sort(left, mid); // 분할한 앞쪽의 리스트를 정렬
-		merge_sort(mid + 1, right); // 뒤쪽의 리스트를 정렬
-		merge(left, mid, right); // 정렬한 2개의 부분 배열을 합병
+	if (left < right) {
+		mid = (left + right) / 2;
+		merge_sort(left, mid);
+		merge_sort(mid + 1, right);
+		merge(left, mid, right);
 	}
 }
 
@@ -56,16 +61,15 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
 	int N;
 	cin >> N;
-	
+
 	for (int i = 0; i < N; i++) {
 		cin >> list[i].first >> list[i].second;
 	}
 
 	merge_sort(0, N - 1);
-	
+
 	for (int i = 0; i < N; i++) {
 		cout << list[i].first << ' ' << list[i].second << '\n';
 	}
