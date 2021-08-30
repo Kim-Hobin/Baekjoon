9f14cad71fda81ef04b14099126b0e6572e0e838
Kim Hobin <20141689@g.dongseo.ac.kr>
2021-08-30 22:57:48 +0900

    init commit:

--- Baekjoon/11047.cpp
+++ Baekjoon/11047.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 #define endl "\n"
 using namespace std;
 
+int cmp(int a, int b) {
+	return a > b;
+}
+
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	int N, K, cnt = 0;
+	cin >> N >> K;
+	vector<int> A(N);
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+	
+	sort(A.begin(), A.end(), cmp);
+	
+	for (int i = 0; i < N; i++) {
+		while (K - A[i] >= 0) {
+			cnt++;
+			K -= A[i];
+		}
+	}
+
+	cout << cnt << endl;
 
 	return 0;
 }
--- Baekjoon/1931.cpp
+++ Baekjoon/1931.cpp
@@ -3,11 +3,9 @@
 #define endl "\n"
 using namespace std;
 
-int main () 
+int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+
 
 	return 0;
 }
